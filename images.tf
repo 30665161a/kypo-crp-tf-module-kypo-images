189ae29d82daadbc0cb60d842504e49d889219f3
BinhanGao <58599467+30665161a@users.noreply.github.com>
2023-05-16 05:22:46 +0800

    Update images.tf

--- images.tf
+++ images.tf
@@ -1,6 +1,6 @@
 resource "openstack_images_image_v2" "focal" {
   name             = "ubuntu-focal-x86_64"
-  image_source_url = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
+  image_source_url = "http://192.168.0.103:8001/focal-server-cloudimg-amd64.img"
   container_format = "bare"
   disk_format      = "qcow2"
 
@@ -11,7 +11,7 @@ resource "openstack_images_image_v2" "focal" {
 
 resource "openstack_images_image_v2" "debian_9" {
   name             = "debian-9-x86_64"
-  image_source_url = "http://cdimage.debian.org/cdimage/openstack/current-9/debian-9-openstack-amd64.qcow2"
+  image_source_url = "http://192.168.0.103:8001/debian-9-openstack-amd64.qcow2"
   container_format = "bare"
   disk_format      = "qcow2"
 
@@ -22,7 +22,7 @@ resource "openstack_images_image_v2" "debian_9" {
 
 resource "openstack_images_image_v2" "debian_10_man" {
   name             = "debian-10-man"
-  image_source_url = "http://cdimage.debian.org/cdimage/openstack/current-10/debian-10-openstack-amd64.qcow2"
+  image_source_url = "http://192.168.0.103:8001/debian-10-openstack-amd64.qcow2"
   container_format = "bare"
   disk_format      = "qcow2"
 
@@ -33,7 +33,7 @@ resource "openstack_images_image_v2" "debian_10_man" {
 
 resource "openstack_images_image_v2" "debian_11_man" {
   name             = "debian-11-man"
-  image_source_url = "https://cloud.debian.org/images/cloud/bullseye/daily/latest/debian-11-genericcloud-amd64-daily.qcow2"
+  image_source_url = "http://192.168.0.103:8001/debian-11-genericcloud-amd64-daily.qcow2"
   container_format = "bare"
   disk_format      = "qcow2"
 
